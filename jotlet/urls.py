f157cc63414083279d5310df11ceaaf477bec7ba
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-01-28 14:41:59 +0000

    Add basic homepage and urls

--- jotlet/urls.py
+++ jotlet/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('boards/', include('boards.urls')),
+    path('', RedirectView.as_view(url='boards/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
