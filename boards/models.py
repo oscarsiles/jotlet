aaa51935800b7011b341796938a1e22164ed9fbc
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-01-30 15:17:38 +0000

    add initial models for boards/topics/posts

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.subject
+
+class Post(models.Model):
+    content = models.TextField(max_length=400)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.content
