88f50194a4b65f75babd506f8ba418a494ebf495
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-04 20:25:22 +0000

    fix to initial startup

--- boards/apps.py
+++ boards/apps.py
@@ -6,9 +6,13 @@ class BoardsConfig(AppConfig):
     name = 'boards'
 
     def ready(self):
-        # important do the import inside the method
-        from django.contrib.auth.models import Group, Permission
+        try:
+            # important do the import inside the method
+            from django.contrib.auth.models import Group, Permission
 
-        moderators, created = Group.objects.get_or_create(name='moderator') # create moderator group
-        permission = Permission.objects.get_or_create(codename='can_delete_post')[0] # get delete post permission permission
-        moderators.permissions.add(permission) # add permission to moderators group
+            moderators, created = Group.objects.get_or_create(name='moderator') # create moderator group
+            permission = Permission.objects.get_or_create(codename='can_delete_post')[0] # get delete post permission permission
+            moderators.permissions.add(permission) # add permission to moderators group
+
+        except:
+            pass
