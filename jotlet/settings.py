c0adf4c506a717f8ac0f6d0e721e4e7b7c9a75e0
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-07 09:12:56 +0000

    add htmx and django-htmx extension

--- jotlet/settings.py
+++ jotlet/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'channels',
     'django_reverse_js',
+    'django_htmx',
     'boards.apps.BoardsConfig',
 ]
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'jotlet.urls'
