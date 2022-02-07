6d3cfdb3121b1d4e36287e5d77f36fcae8ee028f
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-02-07 11:41:29 +0000

    hopefully prevent xss on websocket

--- boards/static/js/board.js
+++ boards/static/js/board.js
@@ -2,7 +2,6 @@ console.log("Sanity check from board.js.");
 
 const board_slug = JSON.parse(document.getElementById('board_slug').textContent);
 const session_key = JSON.parse(document.getElementById('session_key').textContent);
-const can_delete_post = JSON.parse(document.getElementById('can_delete_post').textContent);
 
 var baseUrl = window.location.pathname.split('/')[1] == 'boards' ? window.location.host : window.location.host + "/" + window.location.pathname.split('/')[1];
 var pathName = window.location.pathname.split('/')[1] == 'boards' ? '' : '/' + window.location.pathname.split('/')[1];
@@ -33,7 +32,7 @@ function connect() {
             case "topic_created":
                 var clone = document.importNode(document.querySelector('#topicTemplate').content, true)
                 clone.querySelector('#topic-pk').id = 'topic-' + data.topic_pk;
-                clone.querySelector('#topic-pk-subject').innerHTML = data.topic_subject;
+                clone.querySelector('#topic-pk-subject').innerText = data.topic_subject;
                 clone.querySelector('#topic-pk-subject').id = 'topic-' + data.topic_pk + '-subject';
                 clone.querySelector('#topic-pk-post-create-url').href = pathName + Urls['boards:post-create'](board_slug, data.topic_pk);
                 clone.querySelector('#topic-pk-post-create-url').id = 'topic-' + data.topic_pk + '-post-create-url';
@@ -47,7 +46,7 @@ function connect() {
                 $(clone).insertBefore('#topic-create');
                 break;
             case "topic_updated":
-                $('#topic-' + data.topic_pk + "-subject").html(data.topic_subject);
+                $('#topic-' + data.topic_pk + "-subject").text(data.topic_subject);
                 break;
             case "topic_deleted":
                 document.getElementById('topic-' + data.topic_pk).remove();
@@ -55,7 +54,7 @@ function connect() {
             case "post_created":
                 var clone = document.importNode(document.querySelector('#cardTemplate').content, true)
                 clone.querySelector('#post-pk').id = 'post-' + data.post_pk;
-                clone.querySelector('#post-pk-text').innerHTML = data.post_content;
+                clone.querySelector('#post-pk-text').innerText = data.post_content;
                 clone.querySelector('#post-pk-text').id = 'post-' + data.post_pk + '-text';
                 try {
                     clone.querySelector('#post-pk-update-url').href = pathName + Urls['boards:post_update'](board_slug, data.topic_pk, data.post_pk);
@@ -67,7 +66,7 @@ function connect() {
                 document.getElementById('topic-' + data.topic_pk).appendChild(clone);
                 break;
             case "post_updated":
-                $('#post-' + data.post_pk + "-text").html(data.post_content);
+                $('#post-' + data.post_pk + "-text").text(data.post_content);
                 break;
             case "post_deleted":
                 document.getElementById('post-' + data.post_pk).remove();
