c907066c040cf40ec67e632dcd7a3f13073f1733
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-08 18:21:36 +0000

    - consumers cleanup - add connect/disconnect tracking on page

--- boards/consumers.py
+++ boards/consumers.py
@@ -2,6 +2,7 @@ import json, logging
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.core.cache import cache
 
 from .models import Board, Post, Topic
 
@@ -12,28 +13,56 @@ class BoardConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.board_slug = None
-        self.board = None
         self.board_group_name = None
-        self.topic_pk = None
-        self.post_pk = None
 
     def connect(self):
         self.board_slug = self.scope['url_route']['kwargs']['slug']
-        self.board = Board.objects.get(slug=self.board_slug)
         self.board_group_name = f'board_{self.board_slug}'
 
         self.accept()
+
+        if not cache.get(self.board_group_name):
+            cache.add(self.board_group_name, 0)
+        
+        cache.incr(self.board_group_name)
 
         async_to_sync(self.channel_layer.group_add)(
             self.board_group_name,
             self.channel_name,
         )
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.board_group_name,
+            {
+                'type': 'session_connected',
+                'sessions': cache.get(self.board_group_name),
+            }
+        )
+
     def disconnect(self, code):
+        cache.decr(self.board_group_name)
+
+        if cache.get(self.board_group_name) == 0:
+            cache.delete(self.board_group_name)
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.board_group_name,
+            {
+                'type': 'session_disconnected',
+                'sessions': cache.get(self.board_group_name),
+            }
+        )
+
         async_to_sync(self.channel_layer.group_discard)(
             self.board_group_name,
             self.channel_name,
         )
+
+    def session_connected(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def session_disconnected(self, event):
+        self.send(text_data=json.dumps(event))
 
     def topic_created(self, event):
         self.send(text_data=json.dumps(event))
