682f220bd5ab682e9211a83b393894b50219740d
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-11 17:27:01 +0000

    create board preferences if not exist when editing

--- boards/views.py
+++ boards/views.py
@@ -62,6 +62,9 @@ class BoardPreferencesView(LoginRequiredMixin, UserPassesTestMixin, generic.Upda
 
     def get_object(self): # needed to prevent 'slug' FieldError
         board = Board.objects.get(slug=self.kwargs['slug'])
+        if not BoardPreferences.objects.filter(board=board).exists():
+            board.preferences = BoardPreferences.objects.create(board=board)
+            BoardPreferences.save(board.preferences)
         return board.preferences
 
     def get_form_kwargs(self, **kwargs):
