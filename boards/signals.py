38daa74556d512e5adb8462d23134930c0c5be32
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-12 20:21:36 +0000

    sorl-thumbnail autodelete when main image one is

--- boards/signals.py
+++ boards/signals.py
@@ -1,6 +1,9 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django_cleanup.signals import cleanup_pre_delete
+from sorl.thumbnail import delete
 
 from .models import Board, BoardPreferences
 
@@ -8,3 +11,7 @@ from .models import Board, BoardPreferences
 def create_board_preferences(sender, instance, created, **kwargs):
     if created:
         BoardPreferences.objects.create(board=instance)
+
+@receiver(cleanup_pre_delete)
+def sorl_delete(**kwargs):
+    delete(kwargs['file'])
