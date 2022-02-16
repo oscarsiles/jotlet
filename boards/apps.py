4b26c54e280b806ba4c95dca7d5abff9e05e6ad7
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-16 17:54:30 +0000

    cleanup and move moderators group permissions

--- boards/apps.py
+++ boards/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class BoardsConfig(AppConfig):
@@ -6,21 +7,7 @@ class BoardsConfig(AppConfig):
     name = "boards"
 
     def ready(self):
-        import boards.signals
+        from .signals import populate_models
 
-        try:
-            # important do the import inside the method
-            from django.contrib.auth.models import Group, Permission
-
-            # import your signal file in here if the app is ready
-
-            moderators, created = Group.objects.get_or_create(
-                name="moderator"
-            )  # create moderator group
-            permission = Permission.objects.get_or_create(codename="can_delete_post")[
-                0
-            ]  # get delete post permission permission
-            moderators.permissions.add(permission)  # add permission to moderators group
-
-        except:
-            pass
+        populate_models(sender=self)
+        # post_migrate.connect(populate_models, sender=self)
--- boards/signals.py
+++ boards/signals.py
@@ -1,5 +1,7 @@
 import os
 
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache, caches
 from django.core.cache.utils import make_template_fragment_key
 from django.db.models.signals import post_delete, post_save
@@ -7,9 +9,26 @@ from django.dispatch import receiver
 from django_cleanup.signals import cleanup_pre_delete
 from sorl.thumbnail import delete
 
+from boards.apps import BoardsConfig
+
 from .models import Board, BoardPreferences, Image, Post, Topic
 
 cache_redis = caches["redis-cache"]
+
+
+def populate_models(sender, **kwargs):
+    moderators, created = Group.objects.get_or_create(name="Moderators")
+    content_type = ContentType.objects.get(app_label=BoardsConfig.name, model="post")
+
+    permissions = list(Permission.objects.filter(content_type=content_type))
+
+    custom_permissions = ["add_board"]
+    for custom_perm in custom_permissions:
+        custom_perm = Permission.objects.get(content_type__app_label=BoardsConfig.name, codename=custom_perm)
+        permissions.append(custom_perm)
+
+    for perm in permissions:
+        moderators.permissions.add(perm)
 
 
 @receiver(post_save, sender=Board)
@@ -44,6 +63,7 @@ def post_saved(sender, instance, created, **kwargs):
     except:
         raise Exception(f"Could not delete cache: post-{instance.pk}")
 
+
 @receiver(post_save, sender=Image)
 @receiver(post_delete, sender=Image)
 def update_image_select(sender, instance, **kwargs):
@@ -52,6 +72,7 @@ def update_image_select(sender, instance, **kwargs):
         cache_redis.delete(keyImageSelect)
     except:
         raise Exception(f"Could not delete cache: image_select-{instance.type}")
+
 
 @receiver(cleanup_pre_delete)
 def sorl_delete(**kwargs):
