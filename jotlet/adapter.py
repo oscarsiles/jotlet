ebd1ca9c9c0469f2ca8e07c68d80c2de9c7ca59a
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-17 16:38:02 +0000

    merge if social account signup email matches existing user email

--- jotlet/adapter.py
+++ jotlet/adapter.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import perform_login
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth.models import User
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
@@ -22,6 +24,18 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         allow_signups = super(CustomSocialAccountAdapter, self).is_open_for_signup(request, sociallogin)
         # Override with setting, otherwise default to super.
         return getattr(settings, "SOCIALACCOUNT_ALLOW_SIGNUPS", allow_signups)
+
+    def pre_social_login(self, request, sociallogin):
+        # if user exists, connect the account to the existing account and login
+        user = sociallogin.user
+        if user.id:
+            return
+        try:
+            user = User.objects.get(email=user.email)
+            sociallogin.state["process"] = "connect"
+            perform_login(request, user, "none")
+        except User.DoesNotExist:
+            pass
 
     def populate_user(self, request, sociallogin, data):
         if sociallogin.account.provider == "microsoft":
