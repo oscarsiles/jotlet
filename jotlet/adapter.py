245347e9ff1b544aef00a960d9c237ef082e7f1f
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-17 16:09:18 +0000

    add socialaccount allow signup setting

--- jotlet/adapter.py
+++ jotlet/adapter.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
@@ -11,3 +12,13 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         allow_signups = super(CustomAccountAdapter, self).is_open_for_signup(request)
         # Override with setting, otherwise default to super.
         return getattr(settings, "ACCOUNT_ALLOW_SIGNUPS", allow_signups)
+
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def is_open_for_signup(self, request, sociallogin):
+        """
+        Whether to allow sign ups.
+        """
+        allow_signups = super(CustomSocialAccountAdapter, self).is_open_for_signup(request, sociallogin)
+        # Override with setting, otherwise default to super.
+        return getattr(settings, "SOCIALACCOUNT_ALLOW_SIGNUPS", allow_signups)
