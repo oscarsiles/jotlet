e529344ed2c4dd64d4c348df1ecaa80e3623e193
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-02-28 20:48:46 +0000

    change test settings to use inmemory channel layer

--- jotlet/test_settings.py
+++ jotlet/test_settings.py
@@ -87,12 +87,7 @@ ASGI_APPLICATION = "jotlet.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-            "prefix": "jotlet",
-            "capacity": 500,
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
