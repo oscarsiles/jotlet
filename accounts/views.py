55def90a5dc4d27e7fc4fbf2f4defcc6e6437605
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-03-05 00:01:52 +0000

    change accounts redirect behavior

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,12 @@
-from allauth.account.views import LoginView, PasswordChangeView, PasswordSetView, SignupView
+from allauth.account.views import LoginView, LogoutView, PasswordChangeView, PasswordSetView, SignupView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from django_htmx.http import HttpResponseClientRedirect
+from django_htmx.http import HttpResponseClientRedirect, HttpResponseClientRefresh
 
 
 class JotletLoginView(LoginView):
-    success_url = reverse_lazy("boards:index")
     show_modal = False
 
     def get_context_data(self, **kwargs):
@@ -20,6 +19,12 @@ class JotletLoginView(LoginView):
     def form_valid(self, form):
         response = super().form_valid(form)
         return HttpResponseClientRedirect(self.get_success_url())
+
+
+class JotletLogoutView(LogoutView):
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        return HttpResponseClientRefresh()
 
 
 class JotletSignupView(SignupView):
@@ -38,6 +43,13 @@ class JotletSignupView(SignupView):
 
 
 class JotletChangePasswordView(LoginRequiredMixin, PasswordChangeView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        next_page = self.request.GET.get("next")
+        if next_page:
+            context["next"] = next_page
+        return context
+
     def form_valid(self, form):
         response = super().form_valid(form)
         return HttpResponseClientRedirect(self.get_success_url())
@@ -45,12 +57,19 @@ class JotletChangePasswordView(LoginRequiredMixin, PasswordChangeView):
     def get_success_url(self):
         next_url = self.request.POST.get("next", None)
         if next_url:
-            return "%s" % (next_url)
+            return next_url
         else:
             return reverse_lazy("boards:index")
 
 
 class JotletSetPasswordView(PasswordSetView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        next_page = self.request.GET.get("next")
+        if next_page:
+            context["next"] = next_page
+        return context
+
     def form_valid(self, form):
         response = super().form_valid(form)
         return HttpResponseClientRedirect(self.get_success_url())
@@ -58,6 +77,6 @@ class JotletSetPasswordView(PasswordSetView):
     def get_success_url(self):
         next_url = self.request.POST.get("next", None)
         if next_url:
-            return "%s" % (next_url)
+            return next_url
         else:
             return reverse_lazy("boards:index")
