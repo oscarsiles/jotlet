a3481f2be20e78fd368ffed22a851236ffe89458
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-03-08 16:20:02 +0000

    add create thumbnail task

--- boards/tasks.py
+++ boards/tasks.py
@@ -1,5 +1,11 @@
 from django.core import management
 
+from .models import Image
+
+
+def create_thumbnail(img):
+    return img.get_thumbnail()
+
 
 def thumbnail_cleanup_command():
     return management.call_command("thumbnail", "cleanup")
