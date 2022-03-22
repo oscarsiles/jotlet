b604b053ff5d9ac2fffbafd30f03a2d6c50e8e89
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-03-22 15:25:04 +0000

    implement CSP

---
+++ boards/static/js/components/board_list.js
@@ -0,0 +1,36 @@
+document.getElementById("board-filter-form").removeAttribute("method");
+
+var qInput = document.querySelector("input[name=q]");
+qInput.addEventListener("keyup", (e) => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged");
+});
+
+var beforeInput = document.querySelector("input[name=before]");
+var afterInput = document.querySelector("input[name=after]");
+
+beforeInput.addEventListener("change", (e) => {
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged");
+});
+
+afterInput.addEventListener("change", (e) => {
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged");
+});
+
+var ownerInput = document.querySelector("input[name=owner]");
+tagify = new Tagify(ownerInput, {
+  delimiters: ",| ",
+  originalInputValueFormat: (valuesArr) =>
+    valuesArr.map((item) => item.value).join(","),
+});
+tagify.on("add", (e) =>
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged")
+);
+tagify.on("change", (e) =>
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged")
+);
+tagify.on("remove", (e) =>
+  htmx.trigger(htmx.find("#board-filter-form"), "filterChanged")
+);
