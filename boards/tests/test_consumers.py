82f482a052a496e8d38a13fb83db101fb7ad31cc
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-03-25 14:28:01 +0000

    changes to consumers to hopefully make "live users" more accurate

--- boards/tests/test_consumers.py
+++ boards/tests/test_consumers.py
@@ -1,7 +1,7 @@
 from channels.db import database_sync_to_async
 from channels.routing import URLRouter
 from channels.testing import WebsocketCommunicator
-from django.core.cache import caches
+from django.core.cache import cache
 from django.test import TestCase
 
 from boards.models import Board
@@ -9,8 +9,6 @@ from boards.routing import websocket_urlpatterns
 
 
 class BoardConsumerTest(TestCase):
-    cache = caches["mem-cache"]
-
     @classmethod
     def setUpTestData(cls):
         Board.objects.create(title="Test Board", description="Test Board Description")
@@ -19,7 +17,7 @@ class BoardConsumerTest(TestCase):
         application = URLRouter(websocket_urlpatterns)
         board = await database_sync_to_async(Board.objects.get)(title="Test Board")
         board_group_name = f"board_{board.slug}"
-        self.assertEqual(self.cache.get(board_group_name), None)
+        self.assertEqual(await cache.aget(board_group_name), None)
 
         communicator1 = WebsocketCommunicator(application, f"/ws/boards/{board.slug}/")
         connected, subprotocol = await communicator1.connect()
@@ -28,20 +26,20 @@ class BoardConsumerTest(TestCase):
         message = await communicator1.receive_from()
         self.assertIn("session_connected", message)
         self.assertIn('"sessions": 1', message)
-        self.assertEqual(self.cache.get(board_group_name), 1)
+        self.assertEqual(await cache.aget(board_group_name), 1)
 
         communicator2 = WebsocketCommunicator(application, f"/ws/boards/{board.slug}/")
         connected, subprotocol = await communicator2.connect()
         message = await communicator1.receive_from()
         self.assertIn("session_connected", message)
         self.assertIn('"sessions": 2', message)
-        self.assertEqual(self.cache.get(board_group_name), 2)
+        self.assertEqual(await cache.aget(board_group_name), 2)
 
         await communicator2.disconnect()
         message = await communicator1.receive_from()
         self.assertIn("session_disconnected", message)
         self.assertIn('"sessions": 1', message)
-        self.assertEqual(self.cache.get(board_group_name), 1)
+        self.assertEqual(await cache.aget(board_group_name), 1)
 
         await communicator1.disconnect()
-        self.assertEqual(self.cache.get(board_group_name), None)
+        self.assertEqual(await cache.aget(board_group_name), None)
