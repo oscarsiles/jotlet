8068675c196d1804614906f34372e7703c3418a2
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-04-06 15:33:52 +0100

    separate out footer component add user to reaction and post use User model rather than settings.AUTH_USER_MODEL

--- boards/templatetags/post_extras.py
+++ boards/templatetags/post_extras.py
@@ -6,5 +6,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_sessionkey_reacted(post, session_key, type):
-    return Reaction.objects.filter(post=post, session_key=session_key, type=type).exists()
+def get_sessionkey_reacted(post, request, type):
+    return (
+        Reaction.objects.filter(post=post, session_key=request.session.session_key, type=type).exists()
+        or Reaction.objects.filter(post=post, user=request.user, type=type).exists()
+    )
