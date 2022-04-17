4dbb80c799553789b2c38330a933f5f15fb41a46
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-04-17 18:58:14 +0100

    reduce number of sql queries (first pass)

--- boards/templatetags/post_extras.py
+++ boards/templatetags/post_extras.py
@@ -1,11 +1,9 @@
 from django import template
 
-from boards.utils import get_has_reacted as get_has_reacted_util
-
 register = template.Library()
 
 
 @register.simple_tag
 def get_has_reacted(post, request):
-    has_reacted, reaction_id, reacted_score = get_has_reacted_util(post, request)
+    has_reacted, reaction_id, reacted_score = post.get_has_reacted(request)
     return has_reacted, reaction_id, reacted_score
