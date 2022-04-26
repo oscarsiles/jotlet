6c55cc82dc0e30a5d8c737b94e2b3bf468883110
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-04-26 11:14:29 +0000

    add privacy and terms of use pages

--- jotlet/urls.py
+++ jotlet/urls.py
@@ -19,10 +19,14 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 
+from . import views
+
 urlpatterns = [
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("boards/", include("boards.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("privacy/", views.PrivacyPolicyView.as_view(), name="privacy-policy"),
+    path("terms/", views.TermsOfUseView.as_view(), name="terms-of-use"),
     path("", RedirectView.as_view(url="boards/")),
 ]
 
