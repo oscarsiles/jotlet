2562f8a45cd21abdc6e81b4299c8c8b12f83fac6
Oscar Siles Brugge <48240545+oscarsiles@users.noreply.github.com>
2022-04-27 16:45:31 +0000

    remove linespace

--- boards/utils.py
+++ boards/utils.py
@@ -2,7 +2,6 @@ from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
 
-
 def channel_group_send(group_name, message):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(group_name, message)
