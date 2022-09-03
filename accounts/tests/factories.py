153a1e78faceee68e444474ae48fe0a931116e74
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-09-03 16:58:21 +0000

    add model factories

---
+++ accounts/tests/factories.py
@@ -0,0 +1,16 @@
+import factory
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+test_user_password = "test_password"
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    password = factory.LazyFunction(lambda: make_password(test_user_password))
