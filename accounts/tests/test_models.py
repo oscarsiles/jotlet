fabde135c2cb954444cea83365e387893373aac4
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-09-04 15:41:16 +0000

    update accounts tests

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
+
+from .factories import UserFactory
 
 
 class UserProfileTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user = User.objects.create_user(username="test_user", password="test_password")
-        user.profile.optin_newsletter = True
-        user.profile.save()
+        cls.user = UserFactory()
+        cls.user.profile.optin_newsletter = True
+        cls.user.profile.save()
 
     def test_user_profile_string(self):
-        user = User.objects.get(username="test_user")
-        self.assertEqual(str(user.profile), f"{user.username}'s profile")
+        self.assertEqual(str(self.user.profile), f"{self.user.username}'s profile")
--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -3,57 +3,63 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
+from .factories import USER_TEST_PASSWORD, UserFactory
+
 HCAPTCHA_TEST_RESPONSE = "10000000-aaaa-bbbb-cccc-000000000001"
 
 
 class JotletDeleteViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(username="test", password="test")
-        User.objects.create_user(username="test2", password="test2", is_staff=True)
-        User.objects.create_superuser(username="test3", password="test3")
+        cls.user = UserFactory()
+        cls.user2 = UserFactory(is_staff=True)
+        cls.user3 = UserFactory(is_staff=True, is_superuser=True)
 
     def test_delete_anonymous(self):
         response = self.client.get(reverse("account_delete"))
         self.assertEqual(response.status_code, 302)
 
     def test_delete_user(self):
-        self.client.login(username="test", password="test")
+        self.client.login(username=self.user.username, password=USER_TEST_PASSWORD)
         response = self.client.get(reverse("account_delete"))
         self.assertEqual(response.status_code, 200)
         response = self.client.post(reverse("account_delete"))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("boards:index"))
-        self.assertFalse(User.objects.filter(username="test").exists())
+        self.assertFalse(User.objects.filter(username=self.user.username).exists())
 
     def test_delete_staff(self):
-        self.client.login(username="test2", password="test2")
+        self.client.login(username=self.user2.username, password=USER_TEST_PASSWORD)
         response = self.client.get(reverse("account_delete"))
         self.assertEqual(response.status_code, 200)
         response = self.client.post(reverse("account_delete"))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("account_profile"))
-        self.assertTrue(User.objects.filter(username="test2").exists())
+        self.assertTrue(User.objects.filter(username=self.user2.username).exists())
 
     def test_delete_superuser(self):
-        self.client.login(username="test3", password="test3")
+        self.client.login(username=self.user3.username, password=USER_TEST_PASSWORD)
         response = self.client.get(reverse("account_delete"))
         self.assertEqual(response.status_code, 200)
         response = self.client.post(reverse("account_delete"))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("account_profile"))
-        self.assertTrue(User.objects.filter(username="test3").exists())
+        self.assertTrue(User.objects.filter(username=self.user3.username).exists())
 
 
 class JotletLoginViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(username="test_user", password="test_password")
+        cls.user = UserFactory()
 
     def test_successful_login(self):
         response = self.client.post(
             reverse("account_login"),
-            {"login": "test_user", "password": "test_password", "h-captcha-response": HCAPTCHA_TEST_RESPONSE},
+            {
+                "login": self.user.username,
+                "password": USER_TEST_PASSWORD,
+                "h-captcha-response": HCAPTCHA_TEST_RESPONSE,
+            },
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers["HX-Redirect"], reverse(settings.LOGIN_REDIRECT_URL))
@@ -61,7 +67,11 @@ class JotletLoginViewTest(TestCase):
     def test_hcaptcha_fail(self):
         response = self.client.post(
             reverse("account_login"),
-            {"login": "test_user", "password": "test_password", "h-captcha-response": "incorrect_captcha_response"},
+            {
+                "login": self.user.username,
+                "password": USER_TEST_PASSWORD,
+                "h-captcha-response": "incorrect_captcha_response",
+            },
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
@@ -71,7 +81,11 @@ class JotletLoginViewTest(TestCase):
     def test_incorrect_login(self):
         response = self.client.post(
             reverse("account_login"),
-            {"login": "incorrect_login", "password": "test_password", "h-captcha-response": HCAPTCHA_TEST_RESPONSE},
+            {
+                "login": "incorrect_login",
+                "password": "incorrect_password",
+                "h-captcha-response": HCAPTCHA_TEST_RESPONSE,
+            },
         )
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.context_data["form"].errors)
