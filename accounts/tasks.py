063824372abf4f5ae5cc3b12959bc7dfc817d1ca
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-10-08 10:02:15 +0000

    v1.5.3 - fix reset logs command

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -5,4 +5,4 @@ from huey.contrib.djhuey import db_periodic_task
 
 @db_periodic_task(crontab(minute="5", hour="0"), retries=3)
 def axes_reset_logs():
-    return management.call_command("axes_reset_logs", "30")  # default 30 days
+    return management.call_command("axes_reset_logs", "--age", "30")  # default 30 days
