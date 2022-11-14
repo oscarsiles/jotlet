4c15a01cfc6f48d291c0398c5454fa66c5c49db6
Oscar Siles Brugge <siles.oscar@gmail.com>
2022-11-14 10:47:46 +0000

    enable gzip middleware

--- jotlet/settings.py
+++ jotlet/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
