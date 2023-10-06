1223043193ff5b01b8fb72b416917771471b6d50
Oscar Siles Brügge <siles.oscar@gmail.com>
2023-10-06 21:02:33 +0100

    automatic linting

--- notices/apps.py
+++ notices/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NoticesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notices'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "notices"
