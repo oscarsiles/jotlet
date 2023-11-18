c87bc29c6d42e893ccd815dfdecb95f1f0b53923
Oscar Siles Brügge <siles.oscar@gmail.com>
2023-11-18 17:17:11 +0000

    forgot auto session setting config

--- jotlet/settings.py
+++ jotlet/settings.py
@@ -65,6 +65,7 @@ if SENTRY_ENABLED and not DEBUG and not TESTING:
         integrations=[
             DjangoIntegration(),
         ],
+        auto_session_tracking=env.bool("SENTRY_AUTO_SESSION_TRACKING", default=False),
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
